f66f3a54897e505eb8ac78dcb6ed3442a897160a
technozhantomz <technoz.phantomz@commodity.llc>
2022-02-22 14:40:42 +0300

    USD1.0

--- src/repositories/peerplays.repository.js
+++ src/repositories/peerplays.repository.js
@@ -116,7 +116,7 @@ class PeerplaysRepository {
     if(!isWIF) {
       keys = Login.generateKeys(login, password,
         ['active'],
-        IS_PRODUCTION ? 'USD' : 'LLC');
+        IS_PRODUCTION ? 'USD' : 'USD');
       publicKey = keys.pubKeys.active;
     }
 
@@ -180,7 +180,7 @@ class PeerplaysRepository {
     if(!isWIF) {
       keys = Login.generateKeys(peerplaysAccountName, peerplaysPassword,
         ['active'],
-        IS_PRODUCTION ? 'USD' : 'LLC');
+        IS_PRODUCTION ? 'USD' : 'USD');
       activePrivateKey = keys.privKeys.active;
       activePublicKey = keys.pubKeys.active;
     }
@@ -235,7 +235,7 @@ class PeerplaysRepository {
       if(!isWIF) {
         keys = Login.generateKeys(peerplaysAccountName, peerplaysPassword,
           ['active'],
-          IS_PRODUCTION ? 'USD' : 'LLC');
+          IS_PRODUCTION ? 'USD' : 'USD');
         activePrivateKey = keys.privKeys.active;
         activePublicKey = keys.pubKeys.active;
       }
